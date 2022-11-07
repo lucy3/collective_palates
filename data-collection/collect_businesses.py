e6d0a9054341e58b6b25ed032fcfbd62e8e28d43
Naitian Zhou <naitian@naitian.org>
2022-11-07 11:18:43 -0800

    Use official API

--- data-collection/collect_businesses.py
+++ data-collection/collect_businesses.py
@@ -1,39 +1,56 @@
 """
-Collect a list of businesses for a given ZIP code.
+Collect a list of businesses for a given city.
 """
 import argparse
 import json
 import requests
 
+from tqdm import tqdm
+
 
 MAX_LEN = 10  # max number of results per page
+API_KEY = "5UCj2QJkA45jBWirU00hjLBL_geq-3PFqerB6gWhAjratq2vecmAeJsdl54AYKBLjEDlt9H_mz0K704AoUC_JyTuWH_UMBPax5pPTf1phiwfr4MzYNQS3IjU9UFpY3Yx"
 
-def fetch(zip, start):
+def fetch(city, limit=50, offset=None):
     """Return list of restaurants."""
-    restaurants = []
-    ENDPOINT = f"https://www.yelp.com/search/snippet?find_desc=Restaurants&find_loc={zip}&start={start}"
-    r = requests.get(ENDPOINT).json()
-    if "mainContentComponentsListProps" not in r["searchPageProps"]:
-        return []
-    for section in r["searchPageProps"]["mainContentComponentsListProps"]:
-        if "bizId" in section and not section["searchResultBusiness"]["isAd"]:
-            restaurants.append(section)
+    endpoint = "https://api.yelp.com/v3/businesses/search"
+    params = dict()
+    params["location"] = city
+    params["limit"] = limit
+    if offset is not None:
+        params["offset"] = offset
 
-    return restaurants
+    r = requests.get(
+        endpoint,
+        params,
+        headers={
+            "Authorization": f"Bearer {API_KEY}"
+        }
+    )
+    json_response = r.json()
+    restaurants = json_response["businesses"]
+    total = json_response["total"]
+    return restaurants, total
 
-def main(zip, topk=20, outfile=None):
+
+def main(city, topk, outfile=None):
     if outfile is None:
-        outfile = f"{zip}.jsonl"
+        outfile = f"{city}.jsonl"
 
+    cumulative = 0
+    total = 0
     restaurants = []
-    all = topk == -1
-    start = 0
-    while start < topk or all:
-        results = fetch(zip, start)
+    pbar = tqdm()
+    while cumulative < topk or topk < 0:
+        results, total = fetch(city)
+        pbar.total = min(topk, total) if topk > 0 else total
+        pbar.update(len(results))
+        cumulative += len(results)
         restaurants += results
-        if len(results) < MAX_LEN:
+        if cumulative >= total:
             break
-        start += 10
+    pbar.close()
+    print(f"{len(restaurants)} restaurants out of {total} total")
     with open(outfile, "w") as f:
         for r in restaurants:
             f.write(json.dumps(r) + "\n")
@@ -41,8 +58,8 @@ def main(zip, topk=20, outfile=None):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("zip")
-    parser.add_argument("--topk", default=20, type=int)
+    parser.add_argument("city")
+    parser.add_argument("--topk", default=None, type=int)
     parser.add_argument("--outfile", default=None)
 
     main(**vars(parser.parse_args()))
